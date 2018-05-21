4674ad5bcbcea29b77a18fd6a585f1b0552cfa10
Andrei Tobias <tobias_andrei@yahoo.com>
2018-05-21 16:44:42 +0000

    Generated Models and API Controller

---
+++ app/models/customer.rb
@@ -0,0 +1,2 @@
+class Cutomer < ActiveRecord::Base
+end
---
+++ db/migrate/20180521163734_create_cutomers.rb
@@ -0,0 +1,8 @@
+class CreateCutomers < ActiveRecord::Migration
+  def change
+    create_table :customers do |t|
+
+      t.timestamps null: false
+    end
+  end
+end
