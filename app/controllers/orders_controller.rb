dfdc08f2e06a972cceb4af9bfc1a5e2ec2319fdf
Andrei Tobias <tobias_andrei@yahoo.com>
2018-05-21 18:08:56 +0000

    Modified controller for update, installed bootstrap, added views

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,18 @@ class OrdersController < ApplicationController
     def index
         @orders = Order.all
     end
+    
+    def update
+        @order = Order.find(params[:id])
+        @order.update_attributes!(update_params)
+        respond_to do |format|
+            format.html{redirect_to orders_url}
+            format.js
+        end    
+    end
+    
+    private
+    def update_params
+        params.permit(:status)
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  
+  root 'orders#index'
   namespace :api do
     namespace :v1 do
       resources :orders
     end
   end
+  resources :orders
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
