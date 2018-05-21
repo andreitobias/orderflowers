851903e73290d79c44ebec5660897f664f02f6c2
Andrei Tobias <tobias_andrei@yahoo.com>
2018-05-21 17:40:28 +0000

    Added Controller, Seeds & made order status default pending

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Flower.create(name: 'Tulip', price: Faker::Commerce.price)
+Flower.create(name: 'Rose', price: Faker::Commerce.price)
+Flower.create(name: 'African Daisy', price: Faker::Commerce.price)
+Flower.create(name: 'Alyssum', price: Faker::Commerce.price)
+Flower.create(name: 'Amaryllis', price: Faker::Commerce.price)
+Flower.create(name: 'Peace Lily', price: Faker::Commerce.price)
+Flower.create(name: 'Petunia', price: Faker::Commerce.price)
+Flower.create(name: 'Plume Celosia', price: Faker::Commerce.price)
+10.times do
+    Customer.create({
+        name: Faker::RickAndMorty.character,
+        address: Faker::RickAndMorty.location,
+        email: Faker::Internet.email
+})
+end
