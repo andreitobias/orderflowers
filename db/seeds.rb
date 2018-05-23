bef8a168919ea44ff61f32f20c265c0e11f37c49
Andrei Tobias <tobias_andrei@yahoo.com>
2018-05-23 05:16:38 +0000

    Added new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Flower.create(name: 'Tulip', price: Faker::Commerce.price)
-Flower.create(name: 'Rose', price: Faker::Commerce.price)
-Flower.create(name: 'African Daisy', price: Faker::Commerce.price)
-Flower.create(name: 'Alyssum', price: Faker::Commerce.price)
-Flower.create(name: 'Amaryllis', price: Faker::Commerce.price)
-Flower.create(name: 'Peace Lily', price: Faker::Commerce.price)
-Flower.create(name: 'Petunia', price: Faker::Commerce.price)
-Flower.create(name: 'Plume Celosia', price: Faker::Commerce.price)
+Flower.create(name: 'Tulip', price: 10)
+Flower.create(name: 'Rose', price: 10)
+Flower.create(name: 'African Daisy', price: 10)
+Flower.create(name: 'Alyssum', price: 10)
+Flower.create(name: 'Amaryllis', price: 10)
+Flower.create(name: 'Peace Lily', price: 10)
+Flower.create(name: 'Petunia', price: 10)
+Flower.create(name: 'Plume Celosia', price: 10)
 10.times do
     Customer.create({
-        name: Faker::RickAndMorty.character,
-        address: Faker::RickAndMorty.location,
-        email: Faker::Internet.email
+        name: 'Rick Sanchez',
+        address: 'Earth',
+        email: 'rick@test.com'
 })
 end
